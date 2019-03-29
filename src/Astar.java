62c1d2a3463022e174f56fc4872d492c5ac5ba5f
jonskoberne <jonchisko@gmail.com>
2019-03-29 10:04:57 +0100

    end

--- src/Astar.java
+++ src/Astar.java
@@ -8,10 +8,9 @@ import Rules.Move;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.lang.UnsupportedOperationException;
 import java.security.SecureRandom;
 import java.util.*;
-
+import java.util.concurrent.TimeUnit;
 
 public class Astar {
     /*
@@ -29,86 +28,203 @@ public class Astar {
     -> dont forget to remember the path
      */
 
+    private Chessboard startState;
+    private ArrayList<int[]> enemyKing;
+    private HashMap<String, Integer> fScores = new HashMap<>();
+    private HashMap<Long, String> zobrist2hash = new HashMap<>();
 
-    private static String getPath() throws java.lang.UnsupportedOperationException {
-        throw new java.lang.UnsupportedOperationException("Not implemented");
+    public Astar(Chessboard initialState){
+        this.startState = initialState;
+        this.enemyKing = Astar.enemyKing(initialState);
+    }
+
+    private static String getPath(HashMap<String, Move> hashMoves, Chessboard endNode, Chessboard startNode)  {
+        // returns moves
+        // Chessboard currNode = endNode;
+        StringBuilder sb = new StringBuilder();
+        Move move;
+        String startFEN = startNode.getFEN();
+        String endFEN = endNode.getFEN();
+        while(!endFEN.equals(startFEN)){
+            move = hashMoves.get(endFEN);
+            sb.insert(0, hashMoves.get(endFEN).toString()+";");
+            //sb.append(";");
+            endNode.reverseMove(move);
+            endFEN = endNode.getFEN();
+        }
+        return sb.length() == 0 ? "" : sb.toString().substring(0, sb.length()-1);
     }
 
 
-    private static String findGoal(Chessboard initialState) throws java.lang.UnsupportedOperationException {
-        //throw new java.lang.UnsupportedOperationException("Not implemented");
+    private String findGoal(Chessboard initialState) {
+        long[][] init_zobrist = initiZobrist();
 
         // Chess state comparator
         Comparator<Chessboard> chessStateComparator = (chess1, chess2) -> {
-            return Astar.getF(chess1) - Astar.getF(chess2);
+            return this.getF(chess1) - this.getF(chess2);
         };
 
         // heap where evaluated children are stored, waiting to be visited
         PriorityQueue<Chessboard> heap = new PriorityQueue<>(chessStateComparator);
         // it gives a move that was X state with MOVE to given STATE
-        HashMap<Chessboard, Move> stateMove = new HashMap<>();
+        HashMap<String, Move> stateMove = new HashMap<>();
+        // already seen elements
+        //HashSet<String> seen = new HashSet<>();
 
         heap.add(initialState);
-
-        // game status
-        int gameStatus;
+        Chessboard endState = null;
         // legal moves
         ArrayList<Move> legalMoves;
-        // g path
-        int g;
-        // heuristic path
-        int h;
         // do this while until heap is empty or number of moves reached 0
-        while(!heap.isEmpty()){
+        while(!heap.isEmpty()) {
             // pop minimum evaluated state
             Chessboard bestNode = heap.poll();
-            gameStatus = bestNode.getGameStatus();
-            // the state that was chosen for expansion -> check if goal - check mate, the number of moves left must be 0
-            if(gameStatus == 2 && bestNode.getMovesLeft() == 0){
-                // it is goal
-            }
-            // if node is "remi", dont expand or if no moves left
-            if(gameStatus == 3 || bestNode.getMovesLeft() == 0){
-                continue;
-            }
             // if we made it to here, expand node - get children
             // get legal moves
             legalMoves = bestNode.getMoves();
             // make new states with legal moves
-            for(Move ele : legalMoves){
+            for (Move ele : legalMoves) {
                 // make move
                 bestNode.makeMove(ele);
                 // copy this chess state to child
-                Chessboard child = bestNode.copy();
+                //Chessboard child = bestNode.copy();
+                //System.out.println(bestNode.getFEN() + " " + bestNode.getGameStatus());
+                String hashed = bestNode.getFEN(); //valueZobrist(bestNode, init_zobrist);
+                if(bestNode.getGameStatus() == 1 ||
+                        bestNode.getGameStatus() == 2 && bestNode.getMovesLeft() != 0||
+                        bestNode.getGameStatus() == 3 ||
+                        stateMove.containsKey(hashed)){
+                    // leave this node
+                    //seen.add(hashed); // if not yet in
+                    bestNode.reverseMove(ele);
+                    //System.out.println("Nope");
+                    continue;
+                }
+                if(bestNode.getGameStatus() == 2 && bestNode.getMovesLeft() == 0){
+                    // if checkmate and no moves, this is gut child
+                    stateMove.put(bestNode.getFEN(), ele);
+                    endState = bestNode;
+                    break;
+                }
                 // compute value of f function for created child
                 // TODO:look above
                 // add child to heap
-                heap.add(child);
-                // add child and move to hashMap, add to hashmap states as key and a move that made it
-                stateMove.put(child, ele);
-                // reverse move back to initial bestNode
+                if(!stateMove.containsKey(hashed)) {
+                    heap.add(bestNode.copy());
+                    // add child and move to hashMap, add to hashmap states as key and a move that made it
+                    stateMove.put(bestNode.getFEN(), ele);
+                    // reverse move back to initial bestNode
+                    //seen.add(hashed);
+                }
                 bestNode.reverseMove(ele);
+            }
+            if(endState != null){
+                break; // the loop
             }
 
         }
+        if(endState != null){
+            //System.out.println(endState.getFEN());
+            return Astar.getPath(stateMove, endState ,initialState);
+        }
+        return "No Solution Found";
+    }
 
-
-        return "";
-
-
+    private static ArrayList<int[]> enemyKing(Chessboard state){
+        int player = state.getColor();
+        // you are searching for the opposite king, so opposite colour
+        int king = -6;
+        if(player < 0){
+            king = 6;
+        }
+        int[][] board = state.getBoard();
+        ArrayList<int[]> kingPos = new ArrayList<>();
+        int x, y;
+        for(int indeks = 0; indeks < board.length*board.length; indeks++){
+            y = indeks / board.length;
+            x = indeks % board.length;
+            if(board[y][x] == king) {
+                kingPos.add(new int[]{y-1,x-1});
+                kingPos.add(new int[]{y-1,x});
+                kingPos.add(new int[]{y-1,x+1});
+                kingPos.add(new int[]{y,x-1});
+                kingPos.add(new int[]{y,x+1});
+                kingPos.add(new int[]{y+1,x-1});
+                kingPos.add(new int[]{y+1,x});
+                kingPos.add(new int[]{y+1,x+1});
+                break;
+            }
+        }
+        return kingPos;
     }
 
 
-    private static int getG(Chessboard state) throws java.lang.UnsupportedOperationException {
-        throw new java.lang.UnsupportedOperationException("Not implemented");
+    private int pokritost(Chessboard state){
+        int score = 0;
+        for(int[] field:this.enemyKing){
+            if(field[0] >= 0 && field[1] >= 0){
+                score  += Rules.isCovered(state.getBoard(), field[0], field[1], state.getColor()) ? 1 : 0;
+            }
+        }
+        return -score;
     }
 
-    private static int getH(Chessboard state) throws java.lang.UnsupportedOperationException {
-        throw new java.lang.UnsupportedOperationException("Not implemented");
+    private static int manhattanDistance(int[] pos1, int[] pos2){
+        int xDif = Math.abs(pos1[0] - pos2[0]);
+        int yDif = Math.abs(pos1[1] - pos2[1]);
+        return xDif + yDif;
     }
 
-    private static int getF(Chessboard state) throws java.lang.UnsupportedOperationException {
-        throw new java.lang.UnsupportedOperationException("Not implemented");
+    private int getMin(int[] pos){
+        int min = 1000;
+        int d;
+        for(int[] field:this.enemyKing){
+            d = Astar.manhattanDistance(field, pos);
+            if(min > d){
+                min = d;
+            }
+        }
+        return min;
+    }
+
+    private int manhattanHeuristic(Chessboard state){
+        int player = state.getColor();
+        int[][] board = state.getBoard();
+        int score = 0;
+        int fig;
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                fig = board[i][j];
+                if(player < 0 && fig < 0){
+                    score += this.getMin(new int[]{i, j});
+                }
+                if(player > 0 && fig > 0){
+                    score += this.getMin(new int[]{i, j});
+                }
+            }
+        }
+        return score;
+    }
+
+    private int getPromotion(Chessboard state){
+        return 0;
+    }
+
+
+    private int getH(Chessboard state) {
+        return this.pokritost(state);
+    }
+
+    private int getF(Chessboard state) {
+        int fscore;
+        String fen = state.getFEN();
+        if(this.fScores.containsKey(fen)){
+            fscore = this.fScores.get(fen);
+        }else{
+            fscore = this.getH(state);
+            this.fScores.put(fen, fscore);
+        }
+        return fscore + 1*state.getMovesLeft(); //+ this.manhattanHeuristic(state);
     }
 
     // transpositions, you want to know if state was already  searched
@@ -185,6 +301,27 @@ public class Astar {
 
         // initialize chessboard
         Chessboard currentState = Chessboard.getChessboardFromFEN(currentBoardFEN);
+        Astar alg = new Astar(currentState);
+        String solution = alg.findGoal(currentState);
+        System.out.println(solution);
+
+        /*long t1,t2;
+        for(int i = 1; i <= 60; i++){
+            String fileName = String.valueOf(i)+".txt";
+            f = new File("/Users/jonskoberne/Downloads/progressive_checkmates/"+fileName);
+            try(BufferedReader br = new BufferedReader(new FileReader(f))){
+                System.out.println("File: "+i);
+                currentState = Chessboard.getChessboardFromFEN(br.readLine());
+                alg = new Astar(currentState);
+                t1 = System.nanoTime();
+                System.out.println(alg.findGoal(currentState));
+                System.out.println("Time: "+ (System.nanoTime()-t1)/1000000000);
+            }catch(java.io.FileNotFoundException e){
+                System.out.println(e.getMessage());
+            }catch(Exception e){
+                System.out.println("lol"+e.getMessage());
+            }
+        }*/
 
 
     }
